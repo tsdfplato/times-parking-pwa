625d3b09eaa4a919f0e068935ebb7a30f23c5584
tsdfplato <tsdfplato@users.noreply.github.com>
2026-09-27 04:29:33 +0900

    Force update description to two lines

--- src/App.jsx
+++ src/App.jsx
@@ -553,7 +553,7 @@ function App() {
   </p>
 
   <p className="update-description">
-  <span>空車情報はタイムズ公式サイトから取得。</span>
+  <span>空車情報は公式サイトから取得しています。</span>
   <span>通常は0～10分前の情報を表示します。</span>
 </p>
 </section>
